3ba325c4a829d2bfd0f62826f00a054110441063
md-hasan-ali <hasanaliringku@gmail.com>
2021-11-01 23:52:50 +0600

    some modify

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Adding necessary file 
 const express = require('express')
 const cors = require('cors')
 const { MongoClient } = require('mongodb');
@@ -10,9 +11,11 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+// connected to database 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ayr4p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// server function 
 async function run() {
     try {
         await client.connect();
@@ -20,19 +23,19 @@ async function run() {
         const serviceCollection = database.collection('services')
         const orderServices = database.collection('Orders')
 
-        // delete api 
+        // delete method to orders
         app.delete('/delete/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
-            const result = await orderServices.deleteOne(query)
-            console.log(result)
+            const result1 = await orderServices.deleteOne(query)
+            res.send(result1)
         })
 
         app.delete('/deleteMyorder/:id', async (req, res) => {
             const id = req.params.id;
             const quer = { _id: ObjectId(id) }
             const deleteUser = await orderServices.deleteOne(quer);
-            console.log(deleteUser)
+            res.send(deleteUser)
         })
 
         //get method for service collection
